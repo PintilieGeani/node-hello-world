c9a0ded04e23765d789b158e46ff13e0c6872493
PintilieGeani <pintilie.geany@yahoo.it>
2025-05-21 13:48:02 +0200

    feat: primo-bonus

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Hello Boolean")
+
+
+// Bonus:
+let processArray = process.argv
+let strDaTerminale = []
+for(let i = 2; i < processArray.length; i++){
+    const curElem = processArray[i]
+    strDaTerminale.push(curElem)
+}
+console.log(strDaTerminale.join(" "))
